cd2ad8a83757dcc199ed484e1d7525ce3f199c2f
Uttej Ghanta <uttejmulticloud@gmail.com>
2025-12-12 05:40:24 +0530

    Update AWS instance configuration and names

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ provider "aws" {
 resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0ecb62995f68bb549"
-  instance_type          = "c7i-flex.large"
-  key_name               = "rahamdocker"
-  vpc_security_group_ids = ["sg-046cd16cdb7a78595"]
+  instance_type          = "t3.micro"
+  key_name               = "Swiggy1"
+  vpc_security_group_ids = ["sg-0835cee6b8de1f54f"]
   tags = {
     Name = var.instance_names[count.index]
   }
 }
 
 variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+  default = ["jenkins1", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
